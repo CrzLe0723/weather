6e280ee757c28193a56b31745f6efecfed55c990
Cruz Le <Cruzle0723@gmail.com>
2026-09-07 11:03:11 -0700

    Update simulator.ts

--- simulator.ts
+++ simulator.ts
@@ -1,1 +1,34 @@
+namespace pxsim.Location {
+    let latitudeValue = 0
+    let longitudeValue = 0
+    let readyValue = false
 
+    export function request(): void {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    latitudeValue = position.coords.latitude
+                    longitudeValue = position.coords.longitude
+                    readyValue = true
+                },
+                error => {
+                    console.log("Geolocation error: " + error.message)
+                }
+            )
+        } else {
+            console.log("Geolocation is not supported by this browser.")
+        }
+    }
+
+    export function latitude(): number {
+        return latitudeValue
+    }
+
+    export function longitude(): number {
+        return longitudeValue
+    }
+
+    export function isReady(): boolean {
+        return readyValue
+    }
+}
