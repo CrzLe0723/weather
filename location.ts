49126f712c3042b716930700e3a0982e02559ca0
Cruz Le <Cruzle0723@gmail.com>
2026-09-07 10:50:38 -0700

    Add Location namespace with latitude and longitude functions

---
+++ location.ts
@@ -0,0 +1,12 @@
+//% weight=90 color=#FFAD1F icon="\uf3c5"
+namespace Location {
+    //% block="latitude"
+    export function latitude(): number {
+        return 0
+    }
+
+    //% block="longitude"
+    export function longitude(): number {
+        return 0
+    }
+}
