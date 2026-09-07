41aebcd1cd2fcacbd3c21ec73652752d6bf733bd
Cruz Le <Cruzle0723@gmail.com>
2026-09-07 10:46:52 -0700

    Update temp.ts

--- temp.ts
+++ temp.ts
@@ -1,39 +1,58 @@
-
 /**
- * Custom blocks
+ * Temperature
  */
-//% weight=100 color=#FFAD1F icon="\uf2ca"
+//% weight=90 color=#FFAD1F icon="\uf2ca"
 namespace Temperature {
+
     /**
      * Get the latest temperature in Celsius
-    */
-    //% block
+     */
+    //% block="temperature (°C)"
+    //% group="Temperature"
     export function celsius(): number {
         return 0
     }
 
-    // Get the latest temperature in Fahrenheit
+    /**
+     * Get the latest temperature in Fahrenheit
+     */
+    //% block="temperature (°F)"
+    //% group="Temperature"
     export function fahrenheit(): number {
         return 0
     }
 
-    // Get the latest temperature in Kelvin
+    /**
+     * Get the latest temperature in Kelvin
+     */
+    //% block="temperature (K)"
+    //% group="Temperature"
     export function kelvin(): number {
         return 0
     }
 
-    // Whether temperature data has been obtained
+    /**
+     * Check whether temperature data is available
+     */
+    //% block="temperature data is ready"
+    //% group="Status"
     export function isReady(): boolean {
         return false
     }
 
-    // Start updating temperature data
+    /**
+     * Start updating temperature data
+     */
+    //% block="start temperature updates"
+    //% group="Updates"
     export function start(): void {
-
     }
 
-    // Stop updating temperature data
+    /**
+     * Stop updating temperature data
+     */
+    //% block="stop temperature updates"
+    //% group="Updates"
     export function stop(): void {
-
     }
 }
